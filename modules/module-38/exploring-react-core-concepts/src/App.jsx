68198b441ebbcc1a28b4d77312662e20c9c66213
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-15 22:24:27 +0600

    passing vaule in a component

--- modules/module-38/exploring-react-core-concepts/src/App.jsx
+++ modules/module-38/exploring-react-core-concepts/src/App.jsx
@@ -9,7 +9,9 @@ function App() {
     <>
 
       <h1>Vite + React</h1>
-      <Person></Person>
+      {/* <Person></Person>
+      <Student name="imran" age="23"></Student>
+      <Developer></Developer> */}
 
 
     </>
@@ -27,4 +29,30 @@ function Person() {
   return <h3>I am {name} and {age} years old. My first passion is {passions.first}</h3>
 }
 
+function Student({ name, age }) {
+  console.log(name, age);
+  return (
+    <div className='student'>
+      <h2>This is student container</h2>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+    </div>
+  )
+}
+
+function Developer() {
+  const developerStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '10px'
+  }
+  return (
+    <div style={developerStyle}>
+      <h3>Web App Developer</h3>
+      <p>Coding</p>
+      <p>Programming</p>
+    </div>
+  )
+}
 export default App
