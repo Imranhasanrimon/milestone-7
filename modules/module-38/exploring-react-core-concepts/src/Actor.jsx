476d0a7cc0daaa4ae3b7503b84e814dc70083418
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-16 19:26:35 +0600

    module 38 is done

--- modules/module-38/exploring-react-core-concepts/src/Actor.jsx
+++ modules/module-38/exploring-react-core-concepts/src/Actor.jsx
@@ -1,4 +1,9 @@
 import './actor.css'
+import Todo from './todo'
 export default function Actor({ name }) {
-    return <li className="actor">Name: {name}</li>
+    return (
+        <> <li className="actor">Name: {name}</li>
+            <Todo serial={1} task={'React Documentation'} isDone={true}></Todo>
+        </>
+    )
 }
