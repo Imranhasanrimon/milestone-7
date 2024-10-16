54e397fece5a128968a21ff6a5eff4a85504b00a
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-16 22:15:27 +0600

    learning useState hook

--- modules/module-39/react-core-concept-2/src/App.jsx
+++ modules/module-39/react-core-concept-2/src/App.jsx
@@ -1,17 +1,14 @@
 import './App.css'
+import Counter from './Counter';
 
 function App() {
-  function handleClick() {
-    alert('Button Clicked')
-  }
-  const parameter = (num) => alert(num + 5);
+
   return (
     <>
 
       <h3>React Core Concepts 2</h3>
-      <button onClick={handleClick}>Click Me</button>
-      <button onClick={() => alert('This is from arrow function')}>Arrow</button>
-      <button onClick={() => parameter(5)}>Parameter</button>
+      <Counter></Counter>
+
 
     </>
   )
---
+++ modules/module-39/react-core-concept-2/src/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    const handleCount = () => {
+        const newCount = count + 1;
+        setCount(newCount);
+    }
+    const hadleReduce = () => {
+        const newCount = count - 1;
+        setCount(newCount)
+    }
+
+    return (
+        <div>
+            <h3>Counter: {count}</h3>
+            <button onClick={handleCount}>Add</button>
+            <button onClick={hadleReduce}>Reduce</button>
+        </div>
+    )
+}
