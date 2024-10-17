63556bfa042b2ecd06c580bead898dbbc80bbc66
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-18 00:40:57 +0600

    full player component is done

--- modules/module-39/react-core-concept-2/src/Players.jsx
+++ modules/module-39/react-core-concept-2/src/Players.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react"
+
 export default function Players() {
+    const [player, setPlayer] = useState(11);
+    const handleHire = () => {
+        const newPlayer = player + 1;
+        setPlayer(newPlayer)
+    }
+    const handleCut = () => {
+        setPlayer(player - 1)
+    }
 
     return (
         <div>
-            <h3>Players:</h3>
+            <h3>Players: {player}</h3>
+            <button onClick={handleHire}>Hire</button>
+            <button onClick={handleCut}>Cut</button>
         </div>
     )
 }
