63059d77e512e124dd91b70d847320bce4b7f1f7
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-17 23:25:07 +0600

    exchanging useState hook

--- modules/module-39/react-core-concept-2/src/App.jsx
+++ modules/module-39/react-core-concept-2/src/App.jsx
@@ -2,26 +2,16 @@ import './App.css'
 import User from './User';
 import Friends from './Friends';
 import Counter from './Counter';
-import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleCount = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-  }
-  const handleReduce = () => {
-    const newCount = count - 1;
-    setCount(newCount)
-  }
+
 
 
   return (
     <>
       <h3>React Core Concepts 2</h3>
-      <Counter prop={count}></Counter>
-      <button onClick={handleCount}>Add</button>
-      <button onClick={handleReduce}>Reduce</button>
+      <Counter ></Counter>
+
 
     </>
   )
