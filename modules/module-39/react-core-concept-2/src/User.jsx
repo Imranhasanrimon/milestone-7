96058718544ba5e7f4ea64169dea8cda1413333c
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-17 11:40:00 +0600

    fetching data from json placeholder

--- modules/module-39/react-core-concept-2/src/User.jsx
+++ modules/module-39/react-core-concept-2/src/User.jsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react"
 import { useState } from "react"
 
 export default function User() {
-    const [users, setUsers] = useState()
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
     return (
         <div>
-            <h3>Users:</h3>
+            <h3>Users: {users.length}</h3>
         </div>
     )
 }
