570f47d661c73514c60e6510c25376195a094bcc
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-19 23:10:43 +0600

    applying styles

--- modules/module-40/react-world-tour/src/components/Countries/Country/Country.jsx
+++ modules/module-40/react-world-tour/src/components/Countries/Country/Country.jsx
@@ -1,10 +1,13 @@
 import './country.css'
 const Country = ({ country }) => {
-    const { name, flags } = country;
+    const { name, flags, population, area, cca3 } = country;
     return (
         <div className='country'>
             <p>name: {name.common}</p>
             <img src={flags.png} alt="" />
+            <p>population: {population}</p>
+            <p>area: {area}</p>
+            <p><small>code: {cca3}</small></p>
 
         </div>
     );
