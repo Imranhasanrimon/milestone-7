75be27bfb4ff31546786f8abf6f17a79a0cda936
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-19 15:40:40 +0600

    deleting some component files

--- modules/module-38/exploring-react-core-concepts/src/App.jsx
+++ modules/module-38/exploring-react-core-concepts/src/App.jsx
@@ -1,9 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Todo from './todo'
-import Actor from './Actor'
 
 function App() {
   const products = [
