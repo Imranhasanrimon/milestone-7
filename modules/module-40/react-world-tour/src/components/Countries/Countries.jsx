fa9d21a08fe8594744651ad2a836ccc9a95cd56e
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-20 11:26:55 +0600

    visited onclick function with parameter

--- modules/module-40/react-world-tour/src/components/Countries/Countries.jsx
+++ modules/module-40/react-world-tour/src/components/Countries/Countries.jsx
@@ -9,13 +9,27 @@ const Countries = () => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => setCountries(data))
-    }, [])
+    }, []);
+
+    const [mVisited, setMVisited] = useState([]);
+    const makeList = (countryName) => {
+        const newMVisited = [...mVisited, countryName];
+        setMVisited(newMVisited);
+    }
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h4>visited Countries:</h4>
+                <ul>
+                    {
+                        mVisited.map((name, i) => <li key={i}>{name}</li>)
+                    }
+                </ul>
+            </div>
             <div className="country-container">
                 {
-                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                    countries.map(country => <Country makeList={makeList} key={country.cca3} country={country}></Country>)
                 }
             </div>
         </div>
--- modules/module-40/react-world-tour/src/components/Countries/Country/Country.jsx
+++ modules/module-40/react-world-tour/src/components/Countries/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './country.css'
-const Country = ({ country }) => {
+const Country = ({ country, makeList }) => {
     const { name, flags, population, area, cca3 } = country;
     const [visited, setVisited] = useState(false);
     const handleClick = () => {
@@ -13,6 +13,8 @@ const Country = ({ country }) => {
             <p>population: {population}</p>
             <p>area: {area}</p>
             <p><small>code: {cca3}</small></p>
+            <button onClick={() => makeList(name.common)}>Mark Visited</button>
+            <br />
             <button onClick={handleClick}>{visited ? 'Visited' : 'Going'}</button>
             <span>{visited ? 'I have visited this' : 'I will visit this next year'}</span>
 
