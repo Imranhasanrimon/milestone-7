440e5e3e226854f58de010c45a73d4088dabeb9e
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-20 09:59:44 +0600

    day 2 starting

--- modules/module-40/react-world-tour/src/components/Countries/Country/Country.jsx
+++ modules/module-40/react-world-tour/src/components/Countries/Country/Country.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import './country.css'
 const Country = ({ country }) => {
     const { name, flags, population, area, cca3 } = country;
+    const [visited, setVisited] = useState(false);
+    const handleClick = () => {
+        setVisited(!visited);
+    }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}`}>
             <p>name: {name.common}</p>
             <img src={flags.png} alt="" />
             <p>population: {population}</p>
             <p>area: {area}</p>
             <p><small>code: {cca3}</small></p>
+            <button onClick={handleClick}>{visited ? 'Visited' : 'Going'}</button>
+            <span>{visited ? 'I have visited this' : 'I will visit this next year'}</span>
 
         </div>
     );
