38b29b107b56ad2556c37760d27481291725747d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-21 17:17:15 +0600

    set bottle id to localStorage

--- modules/module-41/memorable-water-bottle/src/Components/Header/Bottle/Bottle.jsx
+++ modules/module-41/memorable-water-bottle/src/Components/Header/Bottle/Bottle.jsx
@@ -1,11 +1,13 @@
 import './Bottle.css'
-const Bottle = ({ bottle }) => {
-    const { name, img, price } = bottle;
+const Bottle = ({ bottle, handleAddToCart }) => {
+    const { name, img, price, id } = bottle;
+
     return (
         < div className="bottle">
             <h3>bottle:{name}</h3>
             <img src={img} alt="" />
             <p>price: {price}</p>
+            <button onClick={() => handleAddToCart(bottle)}>Buy</button>
         </div >
     );
 };
--- modules/module-41/memorable-water-bottle/src/Components/Header/Bottles/Bottles.jsx
+++ modules/module-41/memorable-water-bottle/src/Components/Header/Bottles/Bottles.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
+import { addtoLS } from "../../../utilities/localStorage";
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
@@ -8,13 +9,23 @@ const Bottles = () => {
         fetch('bottle.json')
             .then(res => res.json())
             .then(data => setBottles(data))
-    }, [])
+    }, []);
+
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+        addtoLS(bottle.id)
+    }
+
+
     return (
         <div>
-            <h3>bottles here</h3>
+            <h3>bottles availble: {bottles.length}</h3>
+            <h4>cart: {cart.length}</h4>
             <div className="bottles-container">
                 {
-                    bottles.map((bottle) => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                    bottles.map((bottle) => <Bottle handleAddToCart={handleAddToCart} key={bottle.id} bottle={bottle}></Bottle>)
                 }
             </div>
 
