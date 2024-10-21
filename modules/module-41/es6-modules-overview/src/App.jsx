314194741b675c7682d0737cb1f53e8bcb6d2116
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-21 12:42:59 +0600

    learning object notation

--- modules/module-41/es6-modules-overview/src/App.jsx
+++ modules/module-41/es6-modules-overview/src/App.jsx
@@ -1,19 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Sunglass from './Components/Sunglass/Sunglass'
+import { useEffect, useState } from "react"
+
+import "./App.css"
+import { Watch } from "./Components/Watch/Watch";
 
 function App() {
-
+  // const watches = [
+  //   { id: 1, name: "Rolex Submariner", price: 8000 },
+  //   { id: 2, name: "Omega Seamaster", price: 5000 },
+  //   { id: 3, name: "Casio G-Shock", price: 100 },
+  //   { id: 4, name: "Tag Heuer Carrera", price: 4000 },
+  //   { id: 5, name: "Seiko 5", price: 200 },
+  // ];
+  const [watches, setWatches] = useState([]);
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/Imranhasanrimon/fruit-burst/refs/heads/main/watches.json")
+      .then(res => res.json())
+      .then(data => setWatches(data))
+  }, [])
   return (
     <>
 
       <h1>Vite + React</h1>
-      <Sunglass></Sunglass>
+      {
+        watches.map(watch => <Watch key={watch.id} watch={watch}></Watch>)
+      }
 
     </>
   )
 }
 
 export default App
+const json = [
+  {
+    "name": "imran",
+    "age": 23,
+    "hobbies": ["coding", "reading", "playing"],
+    "isMarried": false,
+    "profession": {
+      "professionName": "web developer",
+      "skills": ["html", "css", "js", "react", "tailwind"],
+      "salary": 20000
+    }
+  }
+]
